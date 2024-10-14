dafabe16e47554f8898fad895922a7b7a6581cc1
Tuka Heba <tuka.heba@gmail.com>
2024-10-14 11:01:18 +0300

    Add Soft Delete Routes

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\RoleController;
+use App\Http\Controllers\Api\UserController;
 use App\Http\Controllers\Api\PermissionController;
 
 /*
@@ -18,15 +19,31 @@ use App\Http\Controllers\Api\PermissionController;
 
 
 // Permission Routes
-Route::apiResource('permissions', PermissionController::class)->middleware('permission:full_access');
+Route::group(['middleware' => ['permission:full_access']], function () {
+    Route::apiResource('permissions', PermissionController::class);
+    Route::get('/permissions/deleted', [PermissionController::class, 'listDeletedPermissions']);
+    Route::post('/permissions/{id}/restore', [PermissionController::class, 'restorePermission']);
+    Route::delete('/permissions/{id}/force-delete', [PermissionController::class, 'forceDeletePermission']);
+});
 
 
 // Role Routes
 Route::group(['middleware' => ['permission:full_access']], function () {
     Route::apiResource('roles', RoleController::class);
+    Route::get('/roles/deleted', [RoleController::class, 'listDeletedRoles']);
+    Route::post('/roles/{id}/restore', [RoleController::class, 'restoreRole']);
+    Route::delete('/roles/{id}/force-delete', [RoleController::class, 'forceDeleteRole']);
     Route::post('roles/{roleId}/permissions', [RoleController::class, 'assignPermissions']);
 });
 
 
 // User Routes
-Route::apiResource('users', PermissionController::class)->middleware('permission:full_access');
+Route::group(['middleware' => ['permission:full_access']], function () {
+    Route::apiResource('users', UserController::class);
+    Route::get('/users/deleted', [UserController::class, 'listDeletedUsers']);
+    Route::post('/users/{id}/restore', [UserController::class, 'restoreUser']);
+    Route::delete('/users/{id}/force-delete', [UserController::class, 'forceDeleteUser']);
+});
+
+// Task Routes
+Route::apiResource('tasks', PermissionController::class);
