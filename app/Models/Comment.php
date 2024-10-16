d6730f8e77bb5479c364926d1ac0c41b75ae03f0
Tuka Heba <tuka.heba@gmail.com>
2024-10-16 23:20:34 +0300

    Add Comment Model

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -8,24 +8,44 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Comment extends Model
 {
-    use HasFactory, SoftDeletes;
+  use HasFactory, SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-      //
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'content',
+    'user_id'
+  ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-      //
-    ];
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array<string, string>
+   */
+  protected $casts = [
+    'user_id' => 'integer',
+  ];
 
+  /**
+   * Relationship to task (polymorphic relationship).
+   * 
+   * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+   */
+  public function commentable()
+  {
+    return $this->morphTo();
+  }
+
+  /**
+   * Relationship to user who write the comment.
+   * 
+   * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+   */
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
 }
