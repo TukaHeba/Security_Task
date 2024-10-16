5dc8f7841d41179bfea076c5ff710e857855baf1
Tuka Heba <tuka.heba@gmail.com>
2024-10-16 23:23:12 +0300

    Update TaskDependency Model

--- app/Models/TaskDependency.php
+++ app/Models/TaskDependency.php
@@ -8,24 +8,44 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TaskDependency extends Model
 {
-    use HasFactory, SoftDeletes;
+  use HasFactory, SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-      //
-    ];    
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'task_id',
+    'depends_on',
+  ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-      //
-    ];
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array<string, string>
+   */
+  protected $casts = [
+    //
+  ];
 
+  /**
+   * Relationship to the task that depends on another task.
+   * 
+   * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+   */
+  public function task()
+  {
+    return $this->belongsTo(Task::class, 'task_id');
+  }
+
+  /**
+   * Relationship to the task that this task depends on.
+   * 
+   * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+   */
+  public function dependsOnTask()
+  {
+    return $this->belongsTo(Task::class, 'depends_on');
+  }
 }
