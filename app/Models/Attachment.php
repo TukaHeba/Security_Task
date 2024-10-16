3ccc32d6651ef32a6711a8640a46d6f1b584388a
Tuka Heba <tuka.heba@gmail.com>
2024-10-16 23:22:13 +0300

    Add Attachment Model

--- app/Models/Attachment.php
+++ app/Models/Attachment.php
@@ -8,24 +8,44 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Attachment extends Model
 {
-    use HasFactory, SoftDeletes;
+  use HasFactory, SoftDeletes;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
-    protected $fillable = [
-      //
-    ];
+  /**
+   * The attributes that are mass assignable.
+   *
+   * @var array<int, string>
+   */
+  protected $fillable = [
+    'type',
+    'user_id',
+  ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-      //
-    ];
+  /**
+   * The attributes that should be cast.
+   *
+   * @var array<string, string>
+   */
+  protected $casts = [
+    'user_id' => 'integer',
+  ];
 
+  /**
+   * Relationship to task (polymorphic relationship).
+   * 
+   * @return \Illuminate\Database\Eloquent\Relations\MorphTo
+   */
+  public function attachable()
+  {
+    return $this->morphTo();
+  }
+
+  /**
+   * Relationship to user who add the attachment.
+   * 
+   * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+   */
+  public function user()
+  {
+    return $this->belongsTo(User::class);
+  }
 }
