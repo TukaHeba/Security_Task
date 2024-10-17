38a9d51cba628906e1209b6cb27edbfca77e7337
Tuka Heba <tuka.heba@gmail.com>
2024-10-17 21:59:57 +0300

    Routes

--- routes/api.php
+++ routes/api.php
@@ -2,8 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\AuthController;
 use App\Http\Controllers\Api\RoleController;
+use App\Http\Controllers\Api\TaskController;
 use App\Http\Controllers\Api\UserController;
+use App\Http\Controllers\Api\ReportController;
+use App\Http\Controllers\Api\ErrorLogController;
 use App\Http\Controllers\Api\PermissionController;
 
 /*
@@ -17,33 +21,59 @@ use App\Http\Controllers\Api\PermissionController;
 |
 */
 
-
-// Permission Routes
-Route::group(['middleware' => ['permission:full_access']], function () {
-    Route::apiResource('permissions', PermissionController::class);
-    Route::get('/permissions/deleted', [PermissionController::class, 'listDeletedPermissions']);
-    Route::post('/permissions/{id}/restore', [PermissionController::class, 'restorePermission']);
-    Route::delete('/permissions/{id}/force-delete', [PermissionController::class, 'forceDeletePermission']);
+// Auth Routes
+Route::controller(AuthController::class)->group(function () {
+    Route::post('login', 'login');
+    Route::post('logout', 'logout')->middleware('auth:api');
+    Route::post('refresh', 'refresh')->middleware('auth:api');
 });
 
+Route::middleware(['throttle:60,1', 'security'])->group(function () {
 
-// Role Routes
-Route::group(['middleware' => ['permission:full_access']], function () {
-    Route::apiResource('roles', RoleController::class);
-    Route::get('/roles/deleted', [RoleController::class, 'listDeletedRoles']);
-    Route::post('/roles/{id}/restore', [RoleController::class, 'restoreRole']);
-    Route::delete('/roles/{id}/force-delete', [RoleController::class, 'forceDeleteRole']);
-    Route::post('roles/{roleId}/permissions', [RoleController::class, 'assignPermissions']);
+    // Permission Routes
+    Route::group(['middleware' => ['permission:full_access']], function () {
+        Route::apiResource('permissions', PermissionController::class);
+        Route::get('/permissions/deleted', [PermissionController::class, 'listDeletedPermissions']);
+        Route::post('/permissions/{id}/restore', [PermissionController::class, 'restorePermission']);
+        Route::delete('/permissions/{id}/force-delete', [PermissionController::class, 'forceDeletePermission']);
+    });
+
+
+    // Role Routes
+    Route::group(['middleware' => ['permission:full_access']], function () {
+        Route::apiResource('roles', RoleController::class);
+        Route::get('/roles/deleted', [RoleController::class, 'listDeletedRoles']);
+        Route::post('/roles/{id}/restore', [RoleController::class, 'restoreRole']);
+        Route::delete('/roles/{id}/force-delete', [RoleController::class, 'forceDeleteRole']);
+        Route::post('roles/{roleId}/permissions', [RoleController::class, 'assignPermissions']);
+    });
+
+
+    // User Routes
+    Route::group(['middleware' => ['permission:full_access']], function () {
+        Route::apiResource('users', UserController::class);
+        Route::get('/users/deleted', [UserController::class, 'listDeletedUsers']);
+        Route::post('/users/{id}/restore', [UserController::class, 'restoreUser']);
+        Route::delete('/users/{id}/force-delete', [UserController::class, 'forceDeleteUser']);
+    });
+
+    // Task Routes
+    Route::controller(TaskController::class)->group(function () {
+        // CRUD
+        Route::apiResource('tasks', TaskController::class)->middleware('permission:task');
+        // sOFT-dELETE
+        Route::get('/tasks/deleted', [TaskController::class, 'listDeletedTasks'])->middleware('permission:task');
+        Route::post('/tasks/{id}/restore', [TaskController::class, 'restoreTask'])->middleware('permission:task');
+        Route::delete('/tasks/{id}/force-delete', [TaskController::class, 'forceDeleteTask'])->middleware('permission:task');
+        // Other Operations
+        Route::post('/tasks/{id}/attachment', [TaskController::class, 'addAttachment'])->middleware('permission:attachment');
+        Route::post('/tasks/{id}/assign', [TaskController::class, 'assignTask'])->middleware('permission:status');
+        Route::put('/tasks/{id}/reassign', [TaskController::class, 'reassignTask'])->middleware('permission:status');
+        Route::put('/tasks/{id}/status', [TaskController::class, 'updateTaskStatus'])->middleware('permission:full_access');
+        Route::get('/tasks/blocked-late', [TaskController::class, 'blockedAndLateTasks'])->middleware('permission:task');
+        Route::post('/tasks/{id}/comment', [TaskController::class, 'addComment'])->middleware('permission:comment');
+    });
+
+    // Report Route
+    Route::get('/reports/daily-tasks', [ReportController::class, 'generateDailyTaskReport'])->middleware('permission:full_access');
 });
-
-
-// User Routes
-Route::group(['middleware' => ['permission:full_access']], function () {
-    Route::apiResource('users', UserController::class);
-    Route::get('/users/deleted', [UserController::class, 'listDeletedUsers']);
-    Route::post('/users/{id}/restore', [UserController::class, 'restoreUser']);
-    Route::delete('/users/{id}/force-delete', [UserController::class, 'forceDeleteUser']);
-});
-
-// Task Routes
-Route::apiResource('tasks', PermissionController::class);
