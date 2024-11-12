9d428ee5f8bab34e5c199cc56efd5fe1aa99e980
Tuka Heba <tuka.heba@gmail.com>
2024-11-12 17:52:29 +0300

    Error Handling

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,8 +2,11 @@
 
 namespace App\Exceptions;
 
-use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Support\Facades\Log;
+use App\Services\ApiResponseService;
+use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -24,7 +27,32 @@ class Handler extends ExceptionHandler
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            //
+            Log::error($e->getMessage(), ['exception' => $e]);
         });
     }
-}
+
+    public function render($request, Throwable $exception)
+    {
+            // Log all exceptions here as well, if desired
+            Log::error('Exception occurred', [
+                'message' => $exception->getMessage(),
+                'file' => $exception->getFile(),
+                'line' => $exception->getLine(),
+                'exception' => $exception,
+            ]);
+
+            if ($exception instanceof NotFoundHttpException) {
+                return ApiResponseService::error(trans('general.resource_not_found'), 404);
+            }
+
+            if ($exception instanceof \Illuminate\Auth\AuthenticationException) {
+                return ApiResponseService::error(trans('general.unauthenticated'), 401);
+            }
+
+            if ($exception instanceof \Illuminate\Auth\Access\AuthorizationException) {
+                return ApiResponseService::error(trans('general.forbidden'), 403);
+            }
+
+            return ApiResponseService::error(trans('general.operation_failed'), 500);
+        }
+    }
